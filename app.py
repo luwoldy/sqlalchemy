2f6149c7777307b0314312729f1b601e8ac83631
luwoldy <leul.woldeab@gmail.com>
2019-02-14 14:07:30 -0500

    Rest of assignment

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-file = 'Data/rainfall.csv'
-rainfall_data = pd.read_csv(file).drop(['Unnamed: 0'], axis=1).to_dict(orient='index')
-
-
+rainfall_file = 'Data/rainfall.csv'
+stations_file = 'Data/stations.csv'
+rainfall_data = pd.read_csv(rainfall_file).drop(['Unnamed: 0'], axis=1).to_dict(orient='index')
+stations_data = pd.read_csv(stations_file).drop(['Unnamed: 0'], axis=1).to_dict(orient='index')
 @app.route("/v1.0/precipitation")
 def precipitation():
 
@@ -25,9 +25,12 @@ def welcome():
     return (
         f"Welcome to the Climate Data API!<br/>"
         f"Available Routes:<br/>"
-        f"/v1.0/precipitation"
+        f"/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations"
     )
 
-
+@app.route("/api/v1.0/stations")
+def stations():
+        return jsonify(stations_data)
 if __name__ == "__main__":
     app.run(debug=True)
